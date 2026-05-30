b787fae7e79adeec4dac22f7ae73ce238dac2829
syntaxMORG0 <noahtveit@icloud.com>
2026-05-30 13:14:48 +0200

    patch

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CC=gcc
-LDFLAGS=
+CFLAGS=-Wall -Wextra -O2
+LDFLAGS=-lX11 -lXcursor
 BUILD_DIR=./build
 SRC_DIR=./src
 
+# build targeted towards Linux
+
 compile:
-	$(CC) $(LDFLAGS) -o $(BUILD_DIR)/main $(SRC_DIR)/*.c
+	mkdir -p $(BUILD_DIR)
+	$(CC) $(CFLAGS) $(SRC_DIR)/*.c -o $(BUILD_DIR)/main $(LDFLAGS)
 
 run:
 	$(BUILD_DIR)/main
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xcursor/Xcursor.h>
+#include <stdlib.h>
 
 int RenderBackground(Display *display, Window window) {
     int color = 0x0000FF;
@@ -21,16 +22,17 @@ int RenderCursor(Display *display, Window window) {
 
 int main() {
     Display *display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        fprintf(stderr, "Cannot open display\n");
+        return 1;
+    }
 
     int screen = DefaultScreen(display);
     Window window = XCreateSimpleWindow(display, RootWindow(display, screen), 10, 10, 800, 600, 1,
                                         BlackPixel(display, screen), WhitePixel(display, screen));
     XMapWindow(display, window);
+    XFlush(display);
 
-    if (display == NULL) {
-        fprintf(stderr, "Cannot open display\n");
-        return 1;
-    }
     RenderBackground(display, window);
     RenderCursor(display, window);
     XCloseDisplay(display);
